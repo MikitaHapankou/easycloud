13d5793dd84e132f6fbb6bdc3e3c9a060faaa7ab
mikitahap <mikitahap@outlook.com>
2026-05-20 22:56:35 +0200

    refactor: move file addition logic from dashboard router to dashboard service

--- app/routers/dashboardRouter.py
+++ app/routers/dashboardRouter.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends, Request, File, UploadFile
+from fastapi import APIRouter, HTTPException, Depends, Request
 from app.config import config
 from fastapi.responses import FileResponse
 import os
@@ -21,22 +21,12 @@ def get_files(user_files: dict = Depends(dashboardService.get_user_files)):
     return user_files
 
 @router.get("/download/{file}")
-def get_dashboard(file: str, user: User = Depends(dependencies.get_current_user)):
-    file_path = os.path.join(config.BASE_DIR, user.login, file)
-    if not os.path.isfile(file_path):
-        raise HTTPException(status_code = 404, detail = "File doesn't exist")
-
+def get_dashboard(file: str, file_path: str = Depends(dashboardService.get_file_path)):
     return FileResponse(file_path, filename = file)
 
 @router.post("/add-file")
-async def add_file(uploaded_file: UploadFile = File(...), user: User = Depends(dependencies.get_current_user)):
-    real_file_path = os.path.join(config.BASE_DIR, user.login, uploaded_file.filename)
-
-    async with aiofiles.open(real_file_path, 'wb') as real_file:
-        while chunk := await uploaded_file.read(1024 * 64):
-            await real_file.write(chunk)
-
-    return "Success"
+async def add_file(result = Depends(dashboardService.add_new_file)):
+    return result
 
 @router.get("/delete-file/{filename}")
 async def delete_file(filename: str, user: User = Depends(dependencies.get_current_user)):
--- app/services/dashboardService.py
+++ app/services/dashboardService.py
@@ -1,20 +1,38 @@
 import os
 from app.models.user import User
 from app.config import config
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, File, UploadFile
 from app import dependencies
+import aiofiles, aiofiles.os
 
 def get_user_files(user: User = Depends(dependencies.get_current_user)):
     user_dir = os.path.join(config.BASE_DIR, user.login)
     if not os.path.exists(user_dir):
-        os.makedirs(user_dir, exist_ok=True)
+        os.makedirs(user_dir, exist_ok = True)
         filenames = []
     else:
         filenames = os.listdir(user_dir)
 
     return {"username": user.login, "files": filenames}
 
-def get_file_path(user: User = Depends(dependencies.get_current_user)):
+def get_file_path(file: str, user: User = Depends(dependencies.get_current_user)):
     file_path = os.path.join(config.BASE_DIR, user.login, file)
+
     if not os.path.isfile(file_path):
-        raise HTTPException(status_code=404, detail="File doesn't exist")
+        raise HTTPException(status_code = 404, detail = "File doesn't exist")
+
+    return file_path
+
+async def add_new_file(uploaded_file: UploadFile = File(...), user: User = Depends(dependencies.get_current_user)):
+    safe_filename = os.path.basename(uploaded_file.filename)
+    real_file_path = os.path.join(config.BASE_DIR, user.login, safe_filename)
+
+    try:
+        async with aiofiles.open(real_file_path, 'wb') as real_file:
+            while chunk := await uploaded_file.read(1024 * 64):
+                await real_file.write(chunk)
+
+        return "Success"
+
+    except Exception:
+        raise HTTPException(status_code = 500, detail = "Couldn't save file")
